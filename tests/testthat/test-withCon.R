2837b8098bb2e944df86a5b414e4df1515c58b7a
Kota Mori <kmori05@gmail.com>
2018-02-03 15:06:22 +0900

    add test cases on withCon and variable scope

--- tests/testthat/test-withCon.R
+++ tests/testthat/test-withCon.R
@@ -5,7 +5,7 @@ library(RSQLite)
 
 context('withCon function with files')
 
-test_that('withCon works with file object', {
+test_that('withCon closes a file object', {
   fname <- tempfile()
   fobj <- file(fname, 'w')
   expect_true(isOpen(fobj), info='file open')
@@ -14,7 +14,7 @@ test_that('withCon works with file object', {
   file.remove(fname)
 })
 
-test_that('withCon works with file object even with error', {
+test_that('withCon closes file object even with error', {
   fname <- tempfile()
   fobj <- file(fname, 'w')
   expect_true(isOpen(fobj), info='file open')
@@ -23,7 +23,7 @@ test_that('withCon works with file object even with error', {
   file.remove(fname)
 })
 
-test_that('withCon works with multiple files', {
+test_that('withCon closes multiple files', {
   f1 <- tempfile()
   f2 <- tempfile()
   fobj1 <- file(f1, 'w')
@@ -39,16 +39,58 @@ test_that('withCon works with multiple files', {
 
 context('withCon with DBI connections')
 
-test_that('withCon works with DBI connections', {
+test_that('withCon closes DBI connection', {
   con <- dbConnect(SQLite(), ':memory:')
   expect_true(dbIsValid(con), info='file open')
   withCon(conn = con, do = {})
   expect_false(dbIsValid(con), info='file closed')
 })
 
-test_that('withCon works with DBI connections with error', {
+test_that('withCon closes DBI connections with error', {
   con <- dbConnect(SQLite(), ':memory:')
   expect_true(dbIsValid(con), info='file open')
   expect_error(withCon(conn = con, do = { foo }), info='error operation')
   expect_false(dbIsValid(con), info='file closed even with error')
 })
+
+
+context('withCon and variable scope')
+
+test_that('withCon operation finds with variables in parent scope', {
+  x <- 10
+  withCon(f = file('foo.txt', 'w'), do = {
+    write(x*5, f)
+  })
+  expect_equal(scan('foo.txt', quiet=TRUE), 50,
+               info='variable in parent env')
+  file.remove('foo.txt')
+
+  x <- 5
+  func <- function() {
+    y <- 3
+    withCon(f = file('foo.txt', 'w'), do = {
+      write(x*y, f)
+    })
+  }
+  func()
+  expect_equal(scan('foo.txt', quiet=TRUE), 15,
+               info='variable in grand parent env')
+  file.remove('foo.txt')
+
+
+  func <- function() {
+    x <- 100
+    function() x
+  }
+  withCon(f = file('foo.txt', 'w'), do = {
+    x <- 0
+    fx <- func()
+    write(fx()*5, f)
+  })
+  expect_equal(scan('foo.txt', quiet=TRUE), 500,
+               info='variable inside function scope')
+  file.remove('foo.txt')
+
+})
+
+
