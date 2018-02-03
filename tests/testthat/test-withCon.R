bd666f174e4e65fa2f327b016a3938d731dd80d7
Kota Mori <kmori05@gmail.com>
2018-02-03 13:32:29 +0900

    add a test case for multiple file objects

--- tests/testthat/test-withCon.R
+++ tests/testthat/test-withCon.R
@@ -3,7 +3,7 @@ library(withcon)
 library(DBI)
 library(RSQLite)
 
-context('withCon function with connections defined outside')
+context('withCon function with files')
 
 test_that('withCon works with file object', {
   fname <- tempfile()
@@ -22,6 +22,22 @@ test_that('withCon works with file object even with error', {
   expect_error(isOpen(fobj), info='file closed even with error')
   file.remove(fname)
 })
+
+test_that('withCon works with multiple files', {
+  f1 <- tempfile()
+  f2 <- tempfile()
+  fobj1 <- file(f1, 'w')
+  fobj2 <- file(f2, 'w')
+  expect_true(isOpen(fobj1), info='file open')
+  expect_true(isOpen(fobj2), info='file open')
+  withCon(f = fobj1, g = fobj2, do = {})
+  expect_error(isOpen(fobj1), info='file closed')
+  expect_error(isOpen(fobj2), info='file closed')
+  file.remove(f1, f2)
+})
+
+
+context('withCon with DBI connections')
 
 test_that('withCon works with DBI connections', {
   con <- dbConnect(SQLite(), ':memory:')
